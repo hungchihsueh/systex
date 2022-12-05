02ba5c5e31899cf6707df110a006670d42da8862
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2022-12-05 11:47:35 +0800

    update

--- pages/components/SideBar.js
+++ pages/components/SideBar.js
@@ -2,17 +2,19 @@ import React from "react";
 import NavButton from "./sidebar/NavButton";
 import Image from "next/image";
 const SideBar = () => {
-  return (
-    <div className="bg-gradient-to-b from-[#63C2CF] to-[#199694] w-1/5 min-w-[276px] h-full px-6 py-9 rounded-3xl">
+    return (
+    <div className="bg-[#4DA7B0] w-1/5 min-w-[276px] h-full pl-6 py-9 rounded-3xl">
       <div className="text-white text-center font-bold mb-20">LOGO</div>
-      <div className="flex flex-col justify-start items-start">
-        <NavButton name="首頁" img="首頁.svg" />
+      <div className="grid grid-cols-1 grid-rows-5 h-4/5">
+        <NavButton name="首頁" img="首頁.svg" isActive={true} />
         <NavButton name="基本設定" img="基本設定.svg" />
         <NavButton name="填報設定" img="填報手冊.svg" />
         <NavButton name="程序清冊" img="程序清冊.svg" />
         <NavButton name="權限管理" img="權限管理.svg" />
-          </div>
-          <Image src="" alt="" ></Image>
+      </div>
+      <button>
+        <Image src="/nav-img/logout.svg" alt="" height="24" width="24"></Image>
+      </button>
     </div>
   );
 };
--- pages/components/sidebar/NavButton.js
+++ pages/components/sidebar/NavButton.js
@@ -2,16 +2,31 @@ import React from "react";
 import Image from "next/image";
 const NavButton = ({ name, img, isActive }) => {
   return (
-    <div className="mb-16">
+    <div className="relative w-full h-fit">
+      {/*  active外部圓角  */}
+    <div className={isActive?"rounded-full w-5 h-5 bg-[#4DA7B0] absolute right-0 bottom-full z-10":"hidden"}></div>
+      <div className={isActive?"h-3 w-3 bg-[#E5F9F5] absolute right-0 bottom-full":"hidden"}></div>
+      {/*  active外部圓角  */}
       <button
         className={
           isActive
-            ? `text-[#4DA7B0] bg-#E5F9F5 rounded-full text-2xl font-medium w-full flex items-center`
+            ? `text-[#4DA7B0] bg-[#E5F9F5] rounded-l-full text-2xl font-medium w-full py-3 px-2 flex items-center before:contents:[] before:w`
             : `text-white text-2xl font-medium w-full flex items-center`
         }>
-              <span><Image src={`/nav-img/${img}`} width="44" height="44" alt="" className={isActive?`mr-4`:`mr-4 brightness-[1000]`}></Image></span>
-        <span>{name}</span>
+        <span>
+          <Image
+            src={`/nav-img/${img}`}
+            width="44"
+            height="44"
+            alt=""
+            className={isActive ? `mr-4` : `mr-4 brightness-[1000]`}></Image>
+        </span>
+        <span className="whitespace-nowrap">{name}</span>
       </button>
+      {/*  active外部圓角  */}
+      <div className={isActive?"rounded-full w-5 h-5 bg-[#4DA7B0] absolute right-0 top-full z-10":"hidden"}></div>
+      <div className={isActive?"h-3 w-3 bg-[#E5F9F5] absolute right-0 top-full":"hidden"}></div>
+      {/*  active外部圓角  */}
     </div>
   );
 };
