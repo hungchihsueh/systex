0c29d218fda1d0fe2fbaed19263760f3a129901f
hungchihsueh <84211143+hungchihsueh@users.noreply.github.com>
2022-12-05 12:31:53 +0800

    update

--- pages/components/SideBar.js
+++ pages/components/SideBar.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import NavButton from "./sidebar/NavButton";
 import Image from "next/image";
 const SideBar = () => {
-    return (
+    const [activeTab, setActiveTab] = useState("首頁");
+    const handleNavButtonClick = (e) => {
+        console.log(e)
+        setActiveTab(e)
+    }
+  return (
     <div className="bg-[#4DA7B0] w-1/5 min-w-[276px] h-full pl-6 py-9 rounded-3xl">
-      <div className="text-white text-center font-bold mb-20">LOGO</div>
+      <div className="text-white text-center font-bold mb-10">LOGO</div>
       <div className="grid grid-cols-1 grid-rows-5 h-4/5">
-        <NavButton name="首頁" img="首頁.svg" isActive={true} />
-        <NavButton name="基本設定" img="基本設定.svg" />
-        <NavButton name="填報設定" img="填報手冊.svg" />
-        <NavButton name="程序清冊" img="程序清冊.svg" />
-        <NavButton name="權限管理" img="權限管理.svg" />
+        <NavButton
+          name="首頁"
+          img="首頁.svg"
+          isActive={activeTab == "首頁"}
+          handleNavButtonClick={handleNavButtonClick}
+        />
+        <NavButton
+          name="基本設定"
+          img="基本設定.svg"
+          isActive={activeTab == "基本設定"}
+          handleNavButtonClick={handleNavButtonClick}
+        />
+        <NavButton
+          name="填報設定"
+          img="填報手冊.svg"
+          isActive={activeTab == "填報設定"}
+          handleNavButtonClick={handleNavButtonClick}
+        />
+        <NavButton
+          name="程序清冊"
+          img="程序清冊.svg"
+          isActive={activeTab == "程序清冊"}
+          handleNavButtonClick={handleNavButtonClick}
+        />
+        <NavButton
+          name="權限管理"
+          img="權限管理.svg"
+          isActive={activeTab == "權限管理"}
+          handleNavButtonClick={handleNavButtonClick}
+        />
       </div>
       <button>
         <Image src="/nav-img/logout.svg" alt="" height="24" width="24"></Image>
--- pages/components/sidebar/NavButton.js
+++ pages/components/sidebar/NavButton.js
@@ -1,17 +1,34 @@
 import React from "react";
 import Image from "next/image";
-const NavButton = ({ name, img, isActive }) => {
+const NavButton = ({
+  name,
+  img,
+  isActive,
+  handleNavButtonClick
+}) => {
   return (
-    <div className="relative w-full h-fit">
+    <div className="relative w-full h-fit transition-all duration-300">
       {/*  active外部圓角  */}
-    <div className={isActive?"rounded-full w-5 h-5 bg-[#4DA7B0] absolute right-0 bottom-full z-10":"hidden"}></div>
-      <div className={isActive?"h-3 w-3 bg-[#E5F9F5] absolute right-0 bottom-full":"hidden"}></div>
-      {/*  active外部圓角  */}
-      <button
+      <div
         className={
           isActive
-            ? `text-[#4DA7B0] bg-[#E5F9F5] rounded-l-full text-2xl font-medium w-full py-3 px-2 flex items-center before:contents:[] before:w`
-            : `text-white text-2xl font-medium w-full flex items-center`
+            ? "rounded-full w-5 h-5 bg-[#4DA7B0] absolute right-0 bottom-full z-10 "
+            : "hidden"
+        }></div>
+      <div
+        className={
+          isActive
+            ? "h-3 w-3 bg-[#E5F9F5] absolute right-0 bottom-full "
+            : "hidden"
+        }></div>
+      {/*  active外部圓角  */}
+      <button
+        type="button"
+        onClick={() => handleNavButtonClick(`${name}`)}
+        className={
+          isActive
+            ? `text-[#4DA7B0] bg-[#E5F9F5] rounded-l-full text-2xl font-medium w-full py-3 px-2 flex items-center`
+            : `text-white text-2xl font-medium w-full py-3 px-2  flex items-center transition-all duration-200`
         }>
         <span>
           <Image
@@ -24,8 +41,18 @@ const NavButton = ({ name, img, isActive }) => {
         <span className="whitespace-nowrap">{name}</span>
       </button>
       {/*  active外部圓角  */}
-      <div className={isActive?"rounded-full w-5 h-5 bg-[#4DA7B0] absolute right-0 top-full z-10":"hidden"}></div>
-      <div className={isActive?"h-3 w-3 bg-[#E5F9F5] absolute right-0 top-full":"hidden"}></div>
+      <div
+        className={
+          isActive
+            ? "rounded-full w-5 h-5 bg-[#4DA7B0] absolute right-0 top-full z-10"
+            : "hideen"
+        }></div>
+      <div
+        className={
+          isActive
+            ? "h-3 w-3 bg-[#E5F9F5] absolute right-0 top-full  opacity-100"
+            : "hidden"
+        }></div>
       {/*  active外部圓角  */}
     </div>
   );
